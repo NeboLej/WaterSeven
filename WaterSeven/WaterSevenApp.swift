41273f4b001295898497922221e57de2888dc173
Nebo <kindersmail08@gmail.com>
2022-10-15 17:45:33 +0300

    init commit

---
+++ WaterSeven/WaterSevenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterSevenApp.swift
+//  WaterSeven
+//
+//  Created by Nebo on 15.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WaterSevenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
