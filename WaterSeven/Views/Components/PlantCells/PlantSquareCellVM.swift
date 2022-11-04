4602bb05ccb67f45a0bbede69304c78d8b817c4e
Nebo <kindersmail08@gmail.com>
2022-11-04 05:56:33 +0300

    created cell

---
+++ WaterSeven/Views/Components/PlantCells/PlantSquareCellVM.swift
@@ -0,0 +1,37 @@
+//
+//  WSPlantSquareCellVM.swift
+//  WaterSeven
+//
+//  Created by Nebo on 04.11.2022.
+//
+
+import Foundation
+
+protocol WSPlantSquareActionProtocol {
+    func onClick(plantId: String)
+    func onClickSuccess(plantId: String)
+}
+
+class WSPlantSquareCellVM: ObservableObject {
+    
+    let id: String
+    let parent: Any?
+    @Published var name: String
+    @Published var comment: String
+    @Published var image: String
+    @Published var isWatering: Bool
+    
+    
+    init(plant: WSPlant, parent: Any?) {
+        id = plant.id
+        name = plant.name
+        comment = plant.comment ?? ""
+        image = plant.image ?? ""
+        isWatering = plant.history.map{ $0.isSameDay(date: Date()) }.contains(true)
+        self.parent = parent
+    }
+    
+    func onClickSuccess() {
+        (parent as! WSPlantSquareActionProtocol).onClickSuccess(plantId: id)
+    }
+}
